fef0258ebc46e9103082dc9cfb4b07c3b15510de
Evgeny Pishulin <evgepishcin@gmail.com>
2024-06-15 00:43:27 +0300

    add api interface ReactiveRedisCache.java

--- src/main/java/org/nullhater/libraries/Main.java
+++
@@ -1,7 +0,0 @@
-package org.nullhater.libraries;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/org/nullhater/libraries/redis/cache/reactive/ReactiveRedisCache.java
@@ -0,0 +1,45 @@
+package org.nullhater.libraries.redis.cache.reactive;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+
+public interface ReactiveRedisCache {
+
+    Mono<Object> get(Object key);
+
+    Mono<Void> put(Object key, Object value);
+
+    Mono<Void> evict(Object key);
+
+    Mono<Void> clear();
+
+    Mono<Void> invalidate();
+
+    default Mono<Object> getMono(Object key) {
+        return get(key);
+    }
+
+    default <T> Mono<T> getMono(Object key, Class<T> clazz) {
+        return getMono(key).cast(clazz);
+    }
+
+    default <T> Mono<T> putMono(Object key, Mono<T> value) {
+        return value.flatMap(data -> put(key, data).thenReturn(data));
+    }
+
+    @SuppressWarnings("unchecked")
+    default Flux<Object> getFlux(Object key) {
+        return get(key).cast(ArrayList.class).flatMapIterable(arrayList -> arrayList);
+    }
+
+    default <T> Flux<T> getFlux(Object key, Class<T> clazz) {
+        return getFlux(key).cast(clazz);
+    }
+
+    default <T> Flux<T> putFlux(Object key, Flux<T> dataStream) {
+        ArrayList<T> data = new ArrayList<>();
+        return dataStream.doOnNext(data::add).concatWith(put(key, data).thenMany(Flux.empty()));
+    }
+}
